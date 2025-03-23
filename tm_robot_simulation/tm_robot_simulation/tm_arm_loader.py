3e57a5ffc6e9ed16f43eeabefaaea8cd3e49fae9
Regina <toto1173250@gmail.com>
2025-03-24 02:43:11 +0800

    Setting initial position for robot arm

--- tm_robot_simulation/tm_robot_simulation/tm_arm_loader.py
+++ tm_robot_simulation/tm_robot_simulation/tm_arm_loader.py
@@ -1,13 +1,38 @@
 import pybullet as p
 import pybullet_data
+import os
+import math
 
 def main():
     physicsClient = p.connect(p.GUI)
     p.setAdditionalSearchPath(pybullet_data.getDataPath())
     p.setGravity(0, 0, -9.8)
-    # robot_id = p.loadURDF("/home/regina/tmdriver_ws/src/tm_description/urdf/tm12-nominal.urdf", useFixedBase=True)
-    # robot_id = p.loadURDF("/home/regina/tmdriver_ws/src/robotiq_arg85_description/robots/robotiq_arg85_description.urdf", useFixedBase=True)
-    robot_id = p.loadURDF("/home/regina/tmdriver_ws/src/tm_description/urdf/tm12_with_gripper.urdf", useFixedBase=True, flags=p.URDF_USE_SELF_COLLISION)
+
+    script_dir = os.path.dirname(os.path.realpath(__file__))
+    urdf_path = os.path.join(script_dir, "../../tm_description/urdf/tm12_with_gripper.urdf")
+    urdf_path = os.path.normpath(urdf_path)
+
+    robot_id = p.loadURDF(urdf_path, useFixedBase=True, flags=p.URDF_USE_SELF_COLLISION)
+
+
+    #Check joint 是否為固定
+    # num_joints = p.getNumJoints(robot_id)
+    # print("Number of joints:", num_joints)
+
+    # for i in range(p.getNumJoints(robot_id)):
+    #     info = p.getJointInfo(robot_id, i)
+    #     joint_name = info[1].decode()
+    #     joint_type = info[2]
+    #     print(f"Joint {i}: name={joint_name}, type={joint_type}")
+
+    # 設定手臂角度
+    arm_joint_positions = [-math.pi/2, -0.5, 1.0, 1.0, 1.5, 0.81]
+    # 0 is based (fixed)
+    arm_joint_indices = [1, 2, 3, 4, 5, 6]
+
+    for i, joint_index in enumerate(arm_joint_indices):
+        p.resetJointState(robot_id, joint_index, arm_joint_positions[i])
+
 
     return robot_id
     
